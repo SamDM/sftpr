ab1866c54c0f67377994524d6d18b2bcb60520dc
Sam De Meyer <samey@psb.vib-ugent.be>
2026-01-06 10:54:26 +0000

    backup 3 most recent unit test dirs

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -43,7 +43,31 @@ sftp_setup <- function(port = 2222) {
 }
 
 sftp_cleanup <- function() {
-  unlink(sftp_test_remote_dir(), recursive = TRUE)
+  test_dir <- sftp_test_remote_dir()
+
+  # Nothing to do if test dir doesn't exist
+  if (!dir.exists(test_dir)) {
+    return(invisible())
+  }
+
+  # Find existing archived test dirs (sftpr-tests-N)
+  existing <- list.files("/tmp", pattern = "^sftpr-tests-[0-9]+$", full.names = TRUE)
+  existing_nums <- as.integer(sub(".*sftpr-tests-", "", existing))
+
+  # Determine next number
+  next_num <- if (length(existing_nums) == 0) 1 else max(existing_nums) + 1
+
+  # Rename current test dir to archived name
+  file.rename(test_dir, sprintf("/tmp/sftpr-tests-%d", next_num))
+
+  # If more than 3 archived dirs, remove all but the most recent 3
+  if (length(existing) >= 3) {
+    sorted_dirs <- existing[order(existing_nums, decreasing = TRUE)]
+    dirs_to_remove <- sorted_dirs[4:length(sorted_dirs)]
+    unlink(dirs_to_remove, recursive = TRUE)
+  }
+
+  invisible()
 }
 
 # Run setup (clean first in case previous run didn't clean up properly)
